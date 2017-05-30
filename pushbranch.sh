c27b43ed71b7d71e3906e5298bb670fab84a5e45
Tony Kelman <tony@kelman.net>
2017-05-30 09:26:04 -0400

    Snippet to also auto open PRs

--- pushbranch.sh
+++ pushbranch.sh
@@ -10,7 +10,21 @@ git checkout -b $newbranch
 git add .
 git commit -m "$commitmsg"
 git remote add mine-oauth https://$(cat ../../token):x-oauth-basic@github.com/$USER/$pkg.jl
-git push mine-oauth $newbranch
+git push mine-oauth +$newbranch
 git remote rm mine-oauth
+
+#owner=$(basename $(dirname $(cat ../METADATA/$pkg/url)))
+#curl -s -H "Authorization: token $(cat ../../token)" \
+#  -d "{\"title\": \"$(echo $commitmsg | head -n1)\", \
+#       \"body\": \"$(echo $commitmsg | tail -n+2)\", \
+#       \"head\": \"$USER:$newbranch\", \
+#       \"base\": \"$oldbranch\", \
+#       \"maintainer_can_modify\": true}" \
+#  https://api.github.com/repos/$owner/$pkg.jl/pulls | jq . #> /dev/null
+# TODO: check if the response says invalid, fail loudly if that happens
+#sleep 3 # avoid making the rate limit too mad
+
 git checkout $oldbranch
+#git show $newbranch | git apply
+git branch -D $newbranch
 cd ..
